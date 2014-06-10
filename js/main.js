f93126f4c8bb02afa75a1e7b4df791060428617a
dynowright <seneres@lycos.com>
2014-06-09 21:06:16 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,13 +1,1 @@
-// pyramid builder
-
-function count(start, end) {
-  for (; start < end; start++) {
-    document.write(start);
-  }
-}
-
-var oldNumber = 1;
-var newNumber = 6;
-count(oldNumber, newNumber);
-count(start, end);
 
