d60ce8df14e5306297a67e3638033f80c127fe26
dynowright <seneres@lycos.com>
2014-06-16 16:12:31 -0400

    Update main.js
    added even odd code (unfinished)

--- js/main.js
+++ js/main.js
@@ -9,3 +9,21 @@ for (i = 0; i < pyramid.length; i++) {
 
 document.getElementById("star").innerHTML = text;
  
+//even odd
+
+var hs = ["ODD", "EVEN", "ODD", "EVEN"];
+text = "";
+
+var i;
+for (i = 0; i < hs.length; i++) {
+    text += hs[i] + "<br>";
+  
+  if (hs[i] % 2 === 0){
+  // I got stuck here!     
+    
+
+    
+  }
+}
+
+document.getElementById("even_or_odd").innerHTML = text;
